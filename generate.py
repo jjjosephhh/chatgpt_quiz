8ec9a5f7919461420dc846906fa0b4cb81b30f23
jo <jo@sus-MacBook-Pro.local>
2023-01-21 08:10:52 +0900

    [FEATURES] Add even more questions about AWS

--- generate.py
+++ generate.py
@@ -5,24 +5,36 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MODEL = "text-davinci-003"
-MAX_TOKENS = 3_000
+MAX_TOKENS = 3_050
 
 prompts = [
-    """
-Create 5 unique multiple choice questions about the content below, each with 5 distinct and unique options, only one of which is correct, with the answer below the options.
-An Availability Zone consists of one or more data centers, each with redundant power, networking, and connectivity. The data centers of a single Availability Zone, or “AZ” for short, are typically within 100 kilometers or 60 miles of each other. Think of it as a cluster of interconnected data centers in a specific geographic zone, that can help your applications become highly available – hence the name, Availability Zone. An AWS Region consists of multiple Availability Zones. AWS has various regions available in North America, South America, Europe, Asia, and other parts of the globe. Since a single availability zone consists of multiple data centers, your system can achieve a higher level of fault-tolerance by running it in two or more availability zones. This enables companies to build highly available, fault-tolerant, and scalable cloud architecture instead of running their applications on a single datacenter. Remember that a single availability zone consists of one or more data centers. Since you can deploy your application or your database to multiple availability zones in a single region, your systems will still be running even if three or more data centers experienced an outage simultaneously. To improve the durability of your data, you can also replicate it to two or more regions. This is helpful for disaster recovery and backups. The Availability Zones of a single AWS Region are typically within hundreds of kilometers or miles of each other. However, these availability zones are still within a specific country to comply with the data sovereignty requirement. This is particularly useful if you have sensitive data that must only be stored in a certain location or country for data privacy compliance.
+"""
+Create 4 unique multiple choice questions about the following content, each with 5 distinct and unique options, only one of which is correct, with the answer below the options.
+There are three fundamental drivers of cost with AWS:
+Compute
+Storage
+Outbound data transfer.
+AWS offers pay-as-you-go for pricing.
+For certain services like Amazon EC2, Amazon EMR, and Amazon RDS, you can invest in reserved capacity. With Reserved Instances, you can save up to 75% over equivalent on-demand capacity. When you buy Reserved Instances, the larger the upfront payment, the greater the discount.
+With the All Upfront option, you pay for the entire Reserved Instance term with one upfront payment. This option provides you with the largest discount compared to On-Demand instance pricing.
+With the Partial Upfront option, you make a low upfront payment and are then charged a discounted hourly rate for the instance for the duration of the Reserved Instance term.
+The No Upfront option does not require any upfront payment and provides a discounted hourly rate for the duration of the term.
+There are also volume-based discounts for services such as Amazon S3.
+For new accounts, AWS Free Tier is available.
+Free Tier offers limited usage of AWS products at no charge for 12 months since the account was created.
+You can estimate your monthly AWS bill using AWS Pricing Calculator.
 """,
 ]
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-with open("aws_cheatsheets.txt", "a") as f:
+with open("example.yml", "a") as f:
     for prompt in prompts:
         try:
             result = openai.Completion.create(
                 model=MODEL,
                 prompt=prompt,
-                temperature=0.5,
+                temperature=0.2,
                 max_tokens=MAX_TOKENS,
                 frequency_penalty=0,
                 presence_penalty=0,
