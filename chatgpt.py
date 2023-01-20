38aa938459f8b8b45641338ab68235867931fa6e
jo <jo@sus-MacBook-Pro.local>
2023-01-21 06:35:26 +0900

    [FEATURE] Add more quiz questions

--- chatgpt.py
+++ chatgpt.py
@@ -1,9 +1,10 @@
 import os
 import openai
 from dotenv import load_dotenv
+
 load_dotenv()
 
-MODEL = 'text-davinci-003'
+MODEL = "text-davinci-003"
 MAX_TOKENS = 3_000
 
 prompts = [
@@ -15,22 +16,22 @@ Create a SVG 100px by 100px icon of a skull wearing a pirate hat.
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 for prompt in prompts:
-    try: 
+    try:
         result = openai.Completion.create(
-                model=MODEL,
-                prompt=prompt,
-                temperature=0.5,
-                max_tokens=MAX_TOKENS,
-                frequency_penalty=0,
-                presence_penalty=0)
-        
-        if result and result.get('choices'):
-            answer = result.get('choices').pop()
-            text = answer.get('text')
+            model=MODEL,
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=MAX_TOKENS,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+
+        if result and result.get("choices"):
+            answer = result.get("choices").pop()
+            text = answer.get("text")
             print(text)
     except Exception as e:
-        print('=============================')
+        print("=============================")
         print(e)
         print(prompt)
-        print('=============================')
-
+        print("=============================")
--- parse.py
+++ parse.py
@@ -1,19 +1,22 @@
+import os
 import yaml
 import json
 
-with open('example.yml', 'r') as stream:
+with open("example.yml", "r") as stream:
     try:
         questions = yaml.safe_load(stream)
         content = []
         for question, answers in questions.items():
             answer = answers.pop()
-            content.append({
-                'question': question,
-                'options': answers,
-                'answer': answer,
-                })
-        content = json.dumps(content, indent=4, ensure_ascii=False) 
-        with open("output.json", "w") as f:
+            content.append(
+                {
+                    "question": question,
+                    "options": answers,
+                    "answer": answer,
+                }
+            )
+        content = json.dumps(content, indent=4, ensure_ascii=False)
+        with open(os.path.join("src", "questions", "questions.json"), "w") as f:
             f.write(content)
     except yaml.YAMLError as exc:
         print(exc)
